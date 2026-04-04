7d9146873620e04982a2f6f5c1e3b6fdad6b625c
Tran Van Minh <tranvanminh110305@gmail.com>
2026-04-04 22:24:50 +0700

    change mysql -> sqlite

--- config/settings.py
+++ config/settings.py
@@ -71,16 +71,8 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database - MySQL
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'ecommerce_db',
-        'USER': 'root',
-        'PASSWORD': '',  # Mật khẩu MySQL của bạn
-        'HOST': 'localhost',
-        'PORT': '3306',
-        'OPTIONS': {
-            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
-            'charset': 'utf8mb4',
-        },
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
